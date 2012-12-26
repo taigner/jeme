c561094a93c205d221ace7d8b0287fab6f402ecf
Tobias Aigner <tobias.aigner@gmail.com>
2012-12-26 15:15:29 +0100

    Files can now be loaded by passing the filename as the program argument

--- src/main/java/com/tobiasaigner/Jeme.java
+++ src/main/java/com/tobiasaigner/Jeme.java
@@ -1,12 +1,36 @@
 package com.tobiasaigner;
 
+import com.tobiasaigner.ast.Primitives;
+import com.tobiasaigner.ast.visitor.PrintVisitor;
+import com.tobiasaigner.ast.visitor.Visitor;
+import com.tobiasaigner.environment.Environment;
+import com.tobiasaigner.environment.Scope;
+import com.tobiasaigner.parser.Parser;
 import com.tobiasaigner.repl.REPL;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 /**
  * @author Tobias Aigner
  */
 public class Jeme {
-    public static void main(String args[]) {
-        new REPL().run();
+    public static void main(String args[]) throws FileNotFoundException {
+        if (args.length == 1)
+            eval(readContentFrom(args[0]));
+        else
+            new REPL().run();
+    }
+
+    private static void eval(String program) {
+        Environment env = new Environment(new Scope("global"));
+        env.addPrimitives(new Primitives());
+        Parser parser = new Parser(new com.tobiasaigner.scanner.Scanner());
+        new PrintVisitor(env).visit(parser.parse(program));
+    }
+
+    private static String readContentFrom(String filename) throws FileNotFoundException {
+        return new Scanner(new File(filename)).useDelimiter("\\Z").next();
     }
 }
